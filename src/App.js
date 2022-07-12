f6b2f4b9219f9998e0ac6be11356f2c86cde9094
Leno rafael <73203800+lenors@users.noreply.github.com>
2022-07-12 11:00:47 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from "react";
+export default class Tela extends Component{
+  render(){
+    return(
+      <div>
+        <h1>Olá, estou aqui batendo cabeça com react, quero terminar logo o curso e ganhar meus 5K por mês
+        </h1>
+      </div>
+    )
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
 }
-
-export default App;
